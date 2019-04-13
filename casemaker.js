e66910b950446e2d04fb3b89b669ddfa11989a0d
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-13 21:45:46 +0000

    Working solution to Case Maker exercise

--- casemaker.js
+++ casemaker.js
@@ -1,5 +1,20 @@
 let camelCase = function(input) {
-  // Your code here
+  var length = input.length;
+  var output = '';
+  if(input[0].toLowerCase()) {
+    output += input[0].toLowerCase();
+  } else {
+    output += input[0];
+  }
+  for(i = 1; i < length; i++) {
+    if(input[i - 1] === ' ') {// if precedding char is a space…
+      output = output.slice(0, -1);// remove said space
+      output += input[i].toUpperCase();// add next letter as capital
+    } else {
+    output += input[i];// otherwise just and next letter
+    }
+  }
+  return output;
 };
 
 console.log(camelCase("this is a string"));
