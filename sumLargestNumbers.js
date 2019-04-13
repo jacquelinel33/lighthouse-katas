bebd350766ec5a9c36510f59d1966c9018a984ee
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-13 18:21:08 +0000

    Updated comment about return element

--- sumLargestNumbers.js
+++ sumLargestNumbers.js
@@ -1,7 +1,7 @@
 let sumLargestNumbers = function(data) {
   var inputarraylength = data.length;
   if(inputarraylength < 2) {
-    return "Not enough numbers in array.";// return false if input array contains less than 2 elements
+    return "Not enough numbers in array.";// return message if input array contains less than 2 elements
   } else {
     data.sort(function(a, b){return b-a});// sort numbers in descending order
     return data[0] + data[1];// adds two largest numbers
