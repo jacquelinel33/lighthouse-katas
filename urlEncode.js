1efa67e923bb9f7907292ddd457a11ad71b6c31d
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-13 20:40:15 +0000

    Working solution to urlEncode exercise

--- urlEncode.js
+++ urlEncode.js
@@ -1,5 +1,24 @@
 const urlEncode = function(text) {
-  // Put your solution here
+  var length = text.length;// get string length
+  var encoded = '';
+  if(text[0] === ' ') {// delete first char if whitespace
+    encoded += '';
+  } else {
+    encoded += text[0];
+  }
+  for(i = 1; i < length - 1; i++) {
+    if(text[i] === ' ') {
+      encoded += '%20';// replace *internal* (note loop start/end points) whitespace with '%20'
+    } else {
+      encoded += text[i];
+    }
+  }
+  if(text[length - 1] === ' ') {// delete last char if whitespace
+    encoded += '';
+  } else {
+    encoded += text[length - 1];
+  }
+  return encoded;
 };
 
 console.log(urlEncode("Lighthouse Labs"));
