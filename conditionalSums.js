e844f518ea05c5cc245b86d89b710b1518f3e5ad
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-13 18:45:10 +0000

    Working solution to conditionalSums exercise

--- conditionalSums.js
+++ conditionalSums.js
@@ -1,5 +1,22 @@
 const conditionalSum = function(values, condition) {
-  // Your code here
+  var inputArraylength = values.length;
+  var sum = 0;
+  if(condition == 'even') {
+    for(i = 0; i < inputArraylength; i ++) {
+      if(values[i] %2 === 0) {
+        sum += values[i];
+      }
+    }
+  } else if(condition == 'odd') {
+    for(i = 0; i < inputArraylength; i ++) {
+      if(values[i] %2 !== 0) {
+        sum += values[i];
+      }
+    }
+  } else {
+    sum = 'Invalid condition';// give error message
+  }
+  return sum;// print out final sum
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
