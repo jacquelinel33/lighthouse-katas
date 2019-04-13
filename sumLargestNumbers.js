93979c95edb3c756972c465d252f9ad692dd428f
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-13 18:19:19 +0000

    Working solution to sumLargestNumbers exercise

--- sumLargestNumbers.js
+++ sumLargestNumbers.js
@@ -1,7 +1,14 @@
 let sumLargestNumbers = function(data) {
-  // Put your solution here
+  var inputarraylength = data.length;
+  if(inputarraylength < 2) {
+    return "Not enough numbers in array.";// return false if input array contains less than 2 elements
+  } else {
+    data.sort(function(a, b){return b-a});// sort numbers in descending order
+    return data[0] + data[1];// adds two largest numbers
+  }
 };
 
+console.log(sumLargestNumbers([1]));
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
