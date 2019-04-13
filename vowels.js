ae11069c5237544681cacdf2c70c86f6697f1bd7
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-13 18:55:45 +0000

    Working solution to vowels exercise

--- vowels.js
+++ vowels.js
@@ -1,5 +1,13 @@
 let numberOfVowels = function(data) {
-  // Put your solution here
+  var length = data.length;
+  var numvowels = 0;
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  for(i = 0; i < length; i++) {
+    if(vowels.includes(data[i])){
+      numvowels += 1;
+    }
+  }
+  return numvowels;
 };
 
 console.log(numberOfVowels("orange"));
