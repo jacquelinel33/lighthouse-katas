a4f58bb3817c4a2e528d9b8bad81c48eb90a2c6c
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-15 03:42:05 +0000

    Added missing )x2

--- organizinginstructors.js
+++ organizinginstructors.js
@@ -7,10 +7,10 @@ console.log(organizeInstructors([
   {name: "Victoria", course: "Web"},
   {name: "Karim", course: "Web"},
   {name: "Donald", course: "Web"}
-]);
+]));
 console.log(organizeInstructors([
   {name: "Brendan", course: "Blockchain"},
   {name: "David", course: "Web"},
   {name: "Martha", course: "iOS"},
   {name: "Carlos", course: "Web"}
-]);
+]));
