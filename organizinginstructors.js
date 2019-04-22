3952eac204c2064e8a15d4d28f1db05d9c63fb1f
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-22 02:37:50 +0000

    Working solution to organizing instructors exercise

--- organizinginstructors.js
+++ organizinginstructors.js
@@ -1,5 +1,22 @@
 const organizeInstructors = function(instructors) {
-  // Put your solution here
+  var output = {};// empty return object
+  var coursenames = [];// empty coursenames array
+  for(var key = 0; key < instructors.length; key++) {
+  var course = instructors[course];
+  if(coursenames.indexOf(instructors[key].course) === -1) {
+  coursenames.push(instructors[key].course);
+  }
+  }// makes list of unique courses
+  //console.log('these are the courses: ' + coursenames);
+  coursenames.forEach(function(course){
+    output[course] = [];// e.g. { iOS: [] }
+    for(var i = 0; i < instructors.length; i++) {
+      if(instructors[i].course === course) {
+      output[course].push(instructors[i].name);
+      }
+    }
+  });
+  return output;
 };
 
 console.log(organizeInstructors([
