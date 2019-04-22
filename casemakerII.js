6d0010573228dce9d51527a527eb66ba16990e4b
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-22 02:41:08 +0000

    Adds case maker II exercise with initial code

---
+++ casemakerII.js
@@ -0,0 +1,11 @@
+const makeCase = function(input, style) {
+  // Put your solution here
+}
+makeCase("this is a string", "camel");
+makeCase("this is a string", "pascal");
+makeCase("this is a string", "snake");
+makeCase("this is a string", "kebab");
+makeCase("this is a string", "title");
+makeCase("this is a string", "vowel");
+makeCase("this is a string", "consonant");
+makeCase("this is a string", ["upper", "snake"]);
