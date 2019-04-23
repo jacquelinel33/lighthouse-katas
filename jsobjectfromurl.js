78747ac7e1e2cc4cf7704a4bb61386ac8442da95
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-23 19:37:19 +0000

    Working solution to Kata 13

--- jsobjectfromurl.js
+++ jsobjectfromurl.js
@@ -1,5 +1,48 @@
 const urlDecode = function(text) {
-  // Put your solution here
+  var output = {};// initialize empty return object
+  var i = 0;
+  var firsttermkey = '';
+  var firsttermvalue = '';
+  var secondtermkey = '';
+  var secondtermvalue = '';
+  while(text[i] !== '=') {
+    firsttermkey += text[i];
+    i++;
+  }// gets first key
+  text = text.substr(i);// delete first key from existing string input
+  i = 0;// reset i to 0
+  while(i < text.length) {
+    if(text[i] === '&') { break; }// exit loop if we have a second key/value pair
+    firsttermvalue += text[i];
+    i++;
+  }
+  firsttermvalue = firsttermvalue.substr(1);// deletes '=' in front of value
+  text = text.substr(i);// delete first key value from existing string input
+
+  for(i = 0; i < firsttermvalue.length; i++) {// change '%20' into ' ' for first term value
+    if(firsttermvalue[i] === '%' && firsttermvalue[i + 1] === '2' && firsttermvalue[i + 2] === '0') {
+      firsttermvalue = firsttermvalue.replace('%20', ' ');// do the swap
+    }
+  }
+  output[firsttermkey] = firsttermvalue;// add to output
+  //console.log(text);
+  i = 0;
+  if(text[0] === '&') {// now do this for two key-value input strings
+    text = text.substr(1);// deletes '&' in front of key
+    while(text[i] !== '=') {
+      secondtermkey += text[i];
+      i++;
+    }// gets second key
+    secondtermvalue = text.substr(i);// delete secondtermkey from text string
+    secondtermvalue = secondtermvalue.substr(1);// delete '=' in front of value
+    for(i = 0; i < secondtermvalue.length; i++) {// change '%20' into ' ' for first term value
+      if(secondtermvalue[i] === '%' && secondtermvalue[i + 1] === '2' && secondtermvalue[i + 2] === '0') {
+        secondtermvalue = secondtermvalue.replace('%20', ' ');// do the swap
+      }
+    }
+    output[secondtermkey] = secondtermvalue;// add to output
+  }
+  return output;
 };
 
 console.log(urlDecode("duck=rubber"));
