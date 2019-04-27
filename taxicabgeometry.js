32bef4897777edb5944e18abfd5dea7098040ce8
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-27 22:10:56 +0000

    Working solution to Kata 16

--- taxicabgeometry.js
+++ taxicabgeometry.js
@@ -1,5 +1,54 @@
 const blocksAway = function(directions) {
-  // Put your solution here
+  var position = {east: 0, north: 0};
+  var cardinaldir;
+  var i;
+
+  if(directions[0] === 'right') {
+    position.east += directions[1];
+    cardinaldir = 'east';
+  } else if(directions[0] === 'left') {
+    position.north += directions[1];
+    cardinaldir = 'north';
+  }
+  // console.log('You are now facing: ' + cardinaldir + ' Current position is east: ' + position.east + ', north: ' + position.north);// establish initial direction
+  for(i = 2; i < directions.length; i += 2) {
+    if(directions[i] === 'right') {// first find new cardinal direction
+      if(cardinaldir === 'north') {
+        cardinaldir = 'east';
+      } else if(cardinaldir === 'east') {
+        cardinaldir = 'south';
+      } else if(cardinaldir === 'south') {
+        cardinaldir = 'west';
+      } else if(cardinaldir === 'west') {
+        cardinaldir = 'north';
+      }
+    } else if(directions[i] === 'left') {
+      if(cardinaldir === 'north') {
+        cardinaldir = 'west';
+      } else if(cardinaldir === 'east') {
+        cardinaldir = 'north';
+      } else if(cardinaldir === 'south') {
+        cardinaldir = 'east';
+      } else if(cardinaldir === 'west') {
+        cardinaldir = 'south';
+      }
+    }
+      if(cardinaldir === 'north') {// now adjust position accordingly
+        position.north += directions[i + 1];
+        // console.log('You are now facing: ' + cardinaldir + ' Current position is east: ' + position.east + ', north:' + position.north);// establish initial direction
+      } else if(cardinaldir === 'east') {
+        position.east += directions[i + 1];
+        // console.log('You are now facing: ' + cardinaldir + ' Current position is east: ' + position.east + ', north:' + position.north);// establish initial direction
+      } else if(cardinaldir === 'south') {
+        position.north -= directions[i + 1];
+        // console.log('You are now facing: ' + cardinaldir + ' Current position is east: ' + position.east + ', north:' + position.north);// establish initial direction
+      } else if(cardinaldir === 'west') {
+        position.east -= directions[i + 1];
+        // console.log('You are now facing: ' + cardinaldir + ' Current position is east: ' + position.east + ', north:' + position.north);// establish initial direction
+      }
+  }
+  // console.log('the length of input array is: ' + directions.length);
+  return position;
 };
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
