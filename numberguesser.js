e297a9fa2a6e2915c2a8df8cb9f267be7169637f
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-27 23:21:17 +0000

    Working solution to number guesser

--- numberguesser.js
+++ numberguesser.js
@@ -1,5 +1,23 @@
 let prompt = require("prompt-sync")();
+const maxNumber = 100;
+let randomNumber = parseInt(Math.random() * maxNumber);// generate random number below max
 
-// code below (replace this example)
-let answer = prompt("What is your name? ");
-console.log("You answered: " + answer);
+const compareNumbers = function(knownNumber) {
+  var attempts = 0;
+  do {
+  var guess = prompt("Guess a number: ");
+  attempts++;
+    if(guess > knownNumber) {
+      console.log('Too High!');
+    } else if(guess < knownNumber) {
+      console.log('Too Low!');
+    } else if(!parseInt(guess)) {
+      console.log('Not a number! Try again!');
+    } else if(guess == knownNumber) {
+      console.log('You got it! You took ' + attempts + ' attempts!');
+    }
+  } while (guess != knownNumber);
+}
+
+//console.log('The random number is: ' + randomNumber);
+compareNumbers(randomNumber);
