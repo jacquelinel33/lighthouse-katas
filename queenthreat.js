660cae9c5eeebc585cf23c57c8e7c53a3007aa63
Russell McWhae <russell.mcwhae@gmail.com>
2019-04-27 14:30:17 +0000

    Addes Kata 15 Queen Threat Detector exercise with initial code

---
+++ queenthreat.js
@@ -0,0 +1,10 @@
+const generateBoard = function() {
+}
+const queenThreat = function() {
+}
+
+let whiteQueen = [0, 5];
+let blackQueen = [5, 0];
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
